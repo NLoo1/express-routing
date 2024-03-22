16b0654e9580fb1c98b066686d9d41de4ce7685d
Nicholas L <NLoo1@outlook.com>
2024-03-21 21:22:07 -0400

    Add itemRoutes

--- itemRoutes.js
+++ itemRoutes.js
@@ -14,8 +14,8 @@ router.get('/', (req,res) => {
 router.post('/', (req, res, next) => {
     try{
         if(!req.body.name) throw new ExpressError('Must include items', 400)
-        const newItem = {name: req.body.name, price: req.body.price}
-        ITEMS.push(req.body.name)
+        const newItem = {name: req.body.name, price: req.body.price || 0}
+        ITEMS.push(newItem)
         return res.status(201).json({item: newItem})
     }
     catch(e){
@@ -24,7 +24,6 @@ router.post('/', (req, res, next) => {
 })
 
 router.get('/:name', (req, res, next) => {
-    console.log(req.params)
     try{
         if(!req.params.name) throw new ExpressError('No name specified.', 400)
         const item = ITEMS.find(c => c.name == req.params.name )
@@ -40,19 +39,19 @@ router.get('/:name', (req, res, next) => {
 router.patch('/:name', (req,res,next) => {
     try{
         // Neither name nor price
-        if(!req.params.name && !req.params.price) throw new ExpressError('No name or price specified.', 400)
+        if(!req.body.name && !req.body.price) throw new ExpressError('No name or price specified.', 400)
         
         // Look for item based on either name or price
-        const item = ITEMS.find(c => c.name == req.params.name || c.name == req.params.price)
+        const item = ITEMS.find(c => c.name == req.params.name)
 
         // if undefined throw error
         if(item === undefined) throw new ExpressError("cannot find item", 404)
         
-        // Check if name
-        if(!req.params.name){
-            item.price = req.params.price
-        } else {
-            item.name = req.params.name
+        if(req.body.name){
+            item.name = req.body.name
+        }  
+        if(req.body.price){
+            item.price = req.body.price
         }
         return res.json({updated: {item: item}})
     } catch(e){
@@ -63,17 +62,14 @@ router.patch('/:name', (req,res,next) => {
 
 router.delete("/:name", function (req, res, next) {
     try {
-        if(!req.params.name) throw new ExpressError('No item specified.', 400)
-        const item = ITEMS.find(c => c.name === +req.params.name || c.name === +req.params.price)
-        if(item === undefined ) throw new ExpressError('Item not found', 400)
-        ITEMS.splice(item, 1)
-        return res.json({message:"Deleted"})
+        const index = ITEMS.findIndex(c => c.name === req.params.name);
+        if(index === -1) throw new ExpressError('Item not found', 400);
+        ITEMS.splice(index, 1);
+        return res.json({message: "Deleted"});
+    } catch (e) {
+        next(e);
     }
-    catch(e){
-        next(e)
-    }
-  })
-
+});
 
 
 module.exports = router
